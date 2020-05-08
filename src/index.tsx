e91e4d61e4ccee6bf3d0f2497d47812eac7e9489
Krajewski Bartosz <bkrajewski@wp-sa.pl>
2020-05-08 09:38:00 +0200

    prettierrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ArrowUrl, { ReactComponent as Arrow } from "./assets/arrow.svg";
-import Pobrane from "./assets/pobrane.jpg";
-import "./index.scss";
-import PropTypes from "prop-types";
-import { Abc } from "./abc/Abc";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ArrowUrl, { ReactComponent as Arrow } from './assets/arrow.svg';
+import Pobrane from './assets/pobrane.jpg';
+import './index.scss';
+import PropTypes from 'prop-types';
+import { Abc } from './abc/Abc';
 
 const App: React.FC = () => {
   return (
@@ -15,9 +15,8 @@ const App: React.FC = () => {
       <img src={Pobrane} />
       <p>
         Api key = {process.env.MODE}
-        We are a most promising species, Mr. Skok, as predators go. Did you know
-        that? I frequently have my doubts. I dont. Not any more. And maybe in a
-        thousand years or so, we will be able to prove it.
+        We are a most promising species, Mr. Skok, as predators go. Did you know that? I frequently have my doubts. I
+        dont. Not any more. And maybe in a thousand years or so, we will be able to prove it.
       </p>
       <p>- Captain X</p>
       <Abc text="witaj klaso" />
@@ -29,4 +28,4 @@ App.propTypes = {
   name: PropTypes.string,
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
